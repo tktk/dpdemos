7e33d0a5de88243abaa9e709da29d41d5fc5f25a
tekuto <g.tekuto@gmail.com>
2013-08-02 17:31:45 +0900

    gamepadにdp::GamePadManager利用コードを追加

--- demos/gamepad/src/main.cpp
+++ demos/gamepad/src/main.cpp
@@ -1,10 +1,38 @@
 ﻿#include "dp/cli.h"
+#include "dp/input/gamepadmanager.h"
+#include "dp/input/gamepadkey.h"
+
+#include <cstdio>
 
 dp::Int dpMain(
     dp::Args &
 )
 {
-    //TODO
+    dp::GamePadManagerInfoPtr   managerInfoPtr( dp::gamePadManagerInfoNew() );
+
+    auto &  managerInfo = *managerInfoPtr;
+
+    dp::gamePadManagerInfoSetConnectEventHandler(
+        managerInfo
+        , [](
+            dp::GamePadManager &        _manager
+            , const dp::GamePadKey &    _KEY
+            , dp::Bool                  _connected
+        )
+        {
+            if( _connected ) {
+                std::printf( "connected\n" );
+            } else {
+                std::printf( "disconnected\n" );
+            }
+        }
+    );
+
+    dp::GamePadManagerPtr   managerPtr( dp::gamePadManagerNew( managerInfo ) );
+
+    std::printf( "Press ENTER to quit\n" );
+
+    std::getchar();
 
     return 0;
 }
