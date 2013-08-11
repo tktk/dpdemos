2f5f78fa1760dc7008526cf619cf29698190fc37
tekuto <g.tekuto@gmail.com>
2013-08-11 18:09:19 +0900

    displayにdp::DisplayManager利用コードを追加

--- demos/display/src/main.cpp
+++ demos/display/src/main.cpp
@@ -1,10 +1,52 @@
 ﻿#include "dp/cli.h"
+#include "dp/display/displaymanager.h"
+#include "dp/display/displaykey.h"
+
+#include <cstdio>
+
+void connectDisplay(
+)
+{
+    //TODO
+    std::printf( "connected\n" );
+}
+
+void disconnectDisplay(
+)
+{
+    //TODO
+    std::printf( "disconnected\n" );
+}
 
 dp::Int dpMain(
     dp::Args &
 )
 {
-    //TODO
+    dp::DisplayManagerInfoUnique    managerInfoUnique( dp::displayManagerInfoNew() );
+
+    auto &  managerInfo = *managerInfoUnique;
+
+    dp::displayManagerInfoSetConnectEventHandler(
+        managerInfo
+        , [](
+            dp::DisplayManager &        _manager
+            , dp::DisplayKeyUnique &&   _keyUnique
+            , dp::Bool                  _connected
+        )
+        {
+            if( _connected ) {
+                connectDisplay();
+            } else {
+                disconnectDisplay();
+            }
+        }
+    );
+
+    dp::DisplayManagerUnique    managerUnique( dp::displayManagerNew( managerInfo ) );
+
+    std::printf( "Press ENTER to quit\n" );
+
+    std::getchar();
 
     return 0;
 }
