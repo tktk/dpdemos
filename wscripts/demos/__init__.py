95cb3d6d5b4a20b6abca46b273959354ebb03e1e
tekuto <g.tekuto@gmail.com>
2013-08-20 21:14:49 +0900

    window(ウィンドウ生成デモ)を追加(仮実装)

--- wscripts/demos/__init__.py
+++ wscripts/demos/__init__.py
@@ -3,8 +3,10 @@
 from . import args
 from . import gamepad
 from . import display
+from . import window
 
 def build( _ctx ):
     args.build( _ctx )
     gamepad.build( _ctx )
     display.build( _ctx )
+    window.build( _ctx )
