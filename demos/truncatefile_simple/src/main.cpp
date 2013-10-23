ca8b01d8980b54ab865ae066233ddf18bd61b995
tekuto <g.tekuto@gmail.com>
2013-10-23 18:08:57 +0900

    truncatefile_simpleの処理を実装

--- demos/truncatefile_simple/src/main.cpp
+++ demos/truncatefile_simple/src/main.cpp
@@ -1,10 +1,76 @@
 ﻿#include "dp/cli.h"
+#include "dp/common/stringconverter.h"
+#include "dp/file/filew.h"
 
-dp::Int dpMain(
-    dp::Args &
+#include <cstdio>
+
+dp::Bool toLong(
+    dp::Long &          _long
+    , const dp::Utf32 & _UTF32
 )
 {
-    //TODO
+    dp::String  str;
+    if( dp::toString(
+        str
+        , _UTF32
+    ) == false ) {
+        return false;
+    }
+
+    dp::StringChar *    endPtr = nullptr;
+    _long = std::strtoll(
+        str.c_str()
+        , &endPtr
+        , 10
+    );
+    if( *endPtr != '\0' ) {
+        return false;
+    }
+
+    return true;
+}
+
+dp::Int dpMain(
+    dp::Args &  _args
+)
+{
+    if( _args.size() < 3 ) {
+        dp::String  command;
+        dp::toString(
+            command
+            , _args[ 0 ]
+        );
+
+        std::printf( "使い方: %s ファイルパス ファイルサイズ\n", command.c_str() );
+
+        return 1;
+    }
+
+    const auto &    FILE_PATH = _args[ 1 ];
+    const auto &    FILE_SIZE_STR = _args[ 2 ];
+
+    dp::Long    fileSize;
+    if( toLong(
+        fileSize
+        , FILE_SIZE_STR
+    ) == false ) {
+        std::printf( "ファイルサイズの数値変換に失敗\n" );
+
+        return 1;
+    }
+
+    auto    fileUnique = dp::unique( dp::newFileA( FILE_PATH ) );
+    if( fileUnique.get() == nullptr ) {
+        std::printf( "dp::FileWの生成に失敗\n" );
+
+        return 1;
+    }
+    auto &  file = *fileUnique;
+
+    dp::truncate(
+        file
+        , fileSize
+    );
 
     return 0;
 }
